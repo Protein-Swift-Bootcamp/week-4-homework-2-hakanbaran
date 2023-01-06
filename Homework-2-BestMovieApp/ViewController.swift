b2ff8c9f58e073905f876454b17f8967f12729f3
Hakan Baran <hakanbaran.developer@gmail.com>
2023-01-06 16:04:56 +0300

    The project has been created...

---
+++ Homework-2-BestMovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework-2-BestMovieApp
+//
+//  Created by Hakan Baran on 6.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
