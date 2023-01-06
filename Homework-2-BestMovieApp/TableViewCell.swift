aa2482b53771326cb9f888ab1ab5a285d32384a8
Hakan Baran <hakanbaran.developer@gmail.com>
2023-01-06 22:51:08 +0300

    Custom Cell is created.

---
+++ Homework-2-BestMovieApp/TableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TableViewCell.swift
+//  Homework-2-BestMovieApp
+//
+//  Created by Hakan Baran on 6.01.2023.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cellImageView: UIImageView!
+    @IBOutlet weak var cellMovieName: UILabel!
+    @IBOutlet weak var cellRating: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
