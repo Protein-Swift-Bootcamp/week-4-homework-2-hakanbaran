eb3c9661ec4627eb9811262795ae0349fad169da
Hakan Baran <hakanbaran.developer@gmail.com>
2023-01-08 01:41:47 +0300

    Add UIActivityIndicatorView(1)...

--- Homework-2-BestMovieApp/View/SecondVC.swift
+++ Homework-2-BestMovieApp/View/SecondVC.swift
@@ -15,14 +15,38 @@ class SecondVC: UIViewController {
     @IBOutlet weak var secondMovieName: UILabel!
     @IBOutlet weak var secondMovieDate: UILabel!
     @IBOutlet weak var secondMovieOverview: UILabel!
-    
     @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var overviewTitleLabel: UILabel!
+    
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
     var viewModel : SecondVCViewModel?
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        spinner.startAnimating()
+        
+        secondImageView.isHidden = true
+        secondMovieName.isHidden = true
+        secondMovieDate.isHidden = true
+        secondMovieOverview.isHidden = true
+        webView.isHidden = true
+        overviewTitleLabel.isHidden = true
+        
+        
+        let seconds = 3.0
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            self.secondVCLoadData()
+        }
+        
+        
+        
+    }
+    
+    func secondVCLoadData() {
+        
         secondMovieName.text = viewModel?.movieName
         secondMovieOverview.text = viewModel?.movieOverView
         
@@ -36,15 +60,10 @@ class SecondVC: UIViewController {
         secondImageView.sd_setImage(with: URL(string: "https://image.tmdb.org/t/p/w500/\(url ?? "")"))
         
         let youtubeVideoID = viewModel?.youtubeView.id.videoId
-        
-        
-        
         guard let youtubeUrl = URL(string: "https://www.youtube.com/embed/\(youtubeVideoID ?? "")") else {
             return
         }
-        
         webView.load(URLRequest(url: youtubeUrl))
-        
         
     }
     
